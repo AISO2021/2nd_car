67fe9b58a7c4df05b75c068217a9063db85c6347
AISO2021 <114710719+AISO2021@users.noreply.github.com>
2022-10-04 17:12:31 +0900

    Add files via upload

--- read.py
+++ read.py
@@ -1,9 +1,27 @@
 import pigpio
+import time
 
 pin_num = 16
+counter = 0
+flag = False
 
 pi=pigpio.pi()
 
 pi.set_mode(pin_num, pigpio.INPUT)
-pi.set_pull_up_down(pin_num, pigpio.PUD_UP)
-print(pi.read(pin_num))
+#pi.set_pull_up_down(pin_num, pigpio.PUD_UP)
+
+while True:
+    sw = pi.read(pin_num)
+    if sw == 0:
+        print('on')
+        if flag == False:
+            counter = 0
+            flag = True
+    if sw == 1:
+        print('off')
+        if flag == True:
+            counter = 0
+            flag = False
+    counter += 1
+    print(counter)
+    
