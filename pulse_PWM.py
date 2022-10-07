470201a02ee00ee1945cc5729ad58abefdd79d92
AISO2021 <114710719+AISO2021@users.noreply.github.com>
2022-10-07 14:27:00 +0900

    Add files via upload

--- pulse_PWM.py
+++ pulse_PWM.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from pickle import TRUE
 import pigpio
 import time
+import threading
 
 DIRpin = 6 #DIRのピン 物理(31)
 PWMpin = 12 #PWMのピン 物理(32)
@@ -42,20 +44,47 @@ def motor(sw):
         pi.write(DIRpin, 1)
 
 
+class Pulse(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.setDaemon(True)
+        self.onr = 0
+        self.st = False
+        self.co = 0
+        self.kill = False
+
+    def count(self, on):
+        while not self.kill:
+            self.onr = on
+            if self.onr == 0:
+                if self.st == False:
+                    self.co += 1
+                    self.st = True
+            if self.onr == 1:
+                self.st = False
+
+            return self.co
+
+
 if __name__ == '__main__':
     print("In The Main Function!")
 
     pi.hardware_PWM(PWMpin, Freq, duty2per(30))
+    p = Pulse()
+    p.start()
 
     try:
         while True:
             motor(1)
-            time.sleep(5)
-            motor(0)
-            time.sleep(5)
+            oho = p.count(pi.read(Pulsepin))
+            print(oho)
+
+            if oho == 42000:
+                pi.hardware_PWM(PWMpin, Freq, duty2per(30))
 
     except (KeyboardInterrupt):
         terminate()
+        p.kill = True
         exit()
 
 
